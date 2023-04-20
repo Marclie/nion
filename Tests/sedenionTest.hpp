db53f712892a83cf0ab57a8b4b8ec5bdafdf5511
Marclie <mliebenthal@fsu.edu>
2023-04-19 22:51:47 -0400

    Improved flag system for including tests of higher order degrees against Dr. Harder's implementations for sedenions and trigintaduonions.

--- Tests/sedenionTest.hpp
+++ Tests/sedenionTest.hpp
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#if TEST_LARGE_DEGREE  == ON
+#ifdef TEST_LARGE_DEGREE
 #ifndef NION_HARDERTEST_HPP
 #define NION_HARDERTEST_HPP
 
